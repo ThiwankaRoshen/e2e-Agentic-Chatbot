7e9665abb1f427e3b203e82d42bdcfc173f67dbc
ThiwankaRoshen <thiwanka.r.pathirana@gmail.com>
2026-06-24 18:04:03 +0530

    create Base app

---
+++ app/agentic_chatbot.py
@@ -0,0 +1,28 @@
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from typing import TypedDict, Annotated
+from dotenv import load_dotenv
+load_dotenv()  
+endpoint = "https://models.github.ai/inference"
+llm = ChatOpenAI(base_url=endpoint,model_name = "openai/gpt-4o-mini")
+
+from langgraph.graph.message import add_messages
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {'messages': [response]}
+
+from langgraph.checkpoint.memory import MemorySaver
+checkpoint = MemorySaver()
+graph = StateGraph(ChatState)
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+chatbot = graph.compile(checkpointer=checkpoint)
+
